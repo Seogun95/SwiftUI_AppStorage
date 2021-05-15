5b17c9da37738d5c5c6ba1eb2b2ba2a2e130e153
SeonJungKim <ksj083895@gmail.com>
2021-05-15 17:56:59 +0500

    Initial Commit

---
+++ Shared/SwiftUI_AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppStorageApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
